0840ca195938ac6de61d44dea4a8b2ad71447106
nickhsine <nickhsine97753017@gmail.com>
2022-05-19 16:28:29 +0800

    refactor(qa): init commit

---
+++ packages/qa/src/build-code/constants.js
@@ -0,0 +1,12 @@
+const namespace = '__readrMediaEmbedCode'
+const pkgName = 'qa'
+
+const webpack = {
+  entryName: 'main',
+}
+
+export default {
+  namespace,
+  pkgName,
+  webpack,
+}
---
+++ packages/qa/src/react-components/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Checkbox from './form/checkbox'
+
+export default function QA(props) {
+  return (
+    <Checkbox title="你是否有以下經歷？（複選）"/>
+  )
+}
