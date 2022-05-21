6e0becef888705c3f762669c60db44756f2d1ef6
caesar <caesar.lee@mirrormedia.mg>
2022-05-21 19:52:18 +0800

    chore(feedback): init commit

---
+++ packages/feedback/dev/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './entry.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    hot: false,
+    host: '0.0.0.0',
+    port: 8080,
+    watchFiles: [path.resolve(__dirname, '../src/app/*.js'), path.resolve(__dirname, '../src/app/**/*.js')],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./index.html"),
+    }),
+  ],
+  devtool: 'eval-source-map',
+}
---
+++ packages/feedback/src/index.js
@@ -0,0 +1,7 @@
+import Feedback from './react-components'
+import { buildEmbeddedCode } from './build-code'
+
+export default {
+  ReactComponent: Feedback,
+  buildEmbeddedCode,
+}
