0b8aa0733a2cd0c43c46d43820cb8656caa47f43
caesar <caesar.lee@mirrormedia.mg>
2022-05-21 20:46:16 +0800

    feat(feedback): add comment form

--- packages/feedback/src/react-components/index.js
+++ packages/feedback/src/react-components/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
+import Section from './layout/Section'
+import CommentForm from './textareaForm/CommentForm'
+
 export default function Feedback() {
+
+  const commentFormSubmitHandler = (textareaValue) => {
+    console.log(`send comment '${textareaValue}' to BE`);
+    return true
+  }
+
   return (
-    <div>Hello READR REACT</div>
+    <>
+      <Section>
+        <CommentForm onSubmit={commentFormSubmitHandler} />
+      </Section>
+    </>
   )
 }
