08d13ea20e4a0d47d6ec7862cdfe842688b290ad
Dian-Yang Fu <85677992+dyfu95@users.noreply.github.com>
2022-05-23 15:07:58 +0800

    style: remove reset-css (#17)

--- packages/qa/src/react-components/index.js
+++ packages/qa/src/react-components/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { ResetStyle, GlobalStyle } from './globalStyle'
+
+/* Because we will use these components as an embed code, 
+ * setting global css here will influence other parts of web site which is not belong to these components.
+ * To prevent the problem mentioned above happen, we decide to remove globalStyle temporary, until we find the solution.
+*/
+
+// import { ResetStyle, GlobalStyle } from './globalStyle'
 
 import Checkbox from './form/checkbox'
 // import Checkbox from './form/checkbox'
 
 export default function QA(props) {
   return (
-    <div>
-      <ResetStyle />
-      <GlobalStyle />
+  
+      /* <ResetStyle />
+      <GlobalStyle /> */
       <Checkbox />
-    </div>
+    
   )
 }
