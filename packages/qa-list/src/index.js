f1ba6e8095c40a9332211e10f4c1e5aece95b4f1
nickhsine <nickhsine97753017@gmail.com>
2022-05-25 11:34:35 +0800

    refactor: export `loadWebpackAssets` function in main script

--- packages/qa-list/src/index.js
+++ packages/qa-list/src/index.js
@@ -1,7 +1,11 @@
 import Feedback from './react-components'
+import path from 'path'
 import { buildEmbeddedCode } from './build-code'
 
 export default {
   ReactComponent: Feedback,
   buildEmbeddedCode,
+  loadWebpackAssets: () => {
+    return require(path.resolve(__dirname, '../dist/webpack-assets.json'))
+  },
 }
