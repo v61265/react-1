140bb8d88743d8aa4bbbec63b48a407b586e5a4c
nickhsine <nickhsine97753017@gmail.com>
2022-05-25 21:20:23 +0800

    fix(questionnaire): wrong mock data

--- packages/questionnaire/dev/mock-data.js
+++ packages/questionnaire/dev/mock-data.js
@@ -1,37 +1,35 @@
 export default {
-  form: {
-    id: "1",
-    name: "好像確診了，怎麼辦？",
-    content: {
-      blocks: [
-        {
-          key: "edm64",
-          data: {},
-          text: "迅速查詢確診相關資訊。這裡放短短的敘述。這裡放短短的敘述。",
-          type: "unstyled",
-          depth: 0,
-          entityRanges: [],
-          inlineStyleRanges: [],
-        },
-      ],
-      entityMap: {},
-    },
-    heroImage: {
-      id: "3",
-      resized: {
-        original:
-          "https://storage.googleapis.com/statics-editools-dev/images/48fb802e-8c15-4d2f-9e5a-262bf355bf22.png",
+  id: "1",
+  name: "好像確診了，怎麼辦？",
+  content: {
+    blocks: [
+      {
+        key: "edm64",
+        data: {},
+        text: "迅速查詢確診相關資訊。這裡放短短的敘述。這裡放短短的敘述。",
+        type: "unstyled",
+        depth: 0,
+        entityRanges: [],
+        inlineStyleRanges: [],
       },
-    },
-    mobileImage: {
-      id: "2",
-      resized: {
-        original:
-          "https://storage.googleapis.com/statics-editools-dev/images/d1a0d1dc-49db-435e-b1a8-6b3bbd4e1263.jpg",
-      },
-    },
-    updateTime: "2022-05-18T16:00:00.000Z",
+    ],
+    entityMap: {},
   },
+  heroImage: {
+    id: "3",
+    resized: {
+      original:
+        "https://storage.googleapis.com/statics-editools-dev/images/48fb802e-8c15-4d2f-9e5a-262bf355bf22.png",
+    },
+  },
+  mobileImage: {
+    id: "2",
+    resized: {
+      original:
+      "https://storage.googleapis.com/statics-editools-dev/images/d1a0d1dc-49db-435e-b1a8-6b3bbd4e1263.jpg",
+    },
+  },
+  updateTime: "2022-05-18T16:00:00.000Z",
   fields: [
     {
       id: "10",
