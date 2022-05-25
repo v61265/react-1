0b1437a23d0b8e91e1d12c3dff1090b929348a81
YiHsuan <v61265@gmail.com>
2022-05-25 16:56:45 +0800

    style(questionnary): adjust style of checkoutbox

--- packages/questionnaire/src/react-components/form/checkbox.js
+++ packages/questionnaire/src/react-components/form/checkbox.js
@@ -1,7 +1,5 @@
-import React  from 'react'
-import styled from 'styled-components'
-
-
+import React from "react";
+import styled from "styled-components";
 
 const Title = styled.h3`
   padding: 16px 0;
@@ -10,40 +8,36 @@ const Title = styled.h3`
   font-weight: 400;
   line-height: 150%;
   box-sizing: border-box;
-  font-family: 'Noto Sans CJK TC', sans-serif;
+  font-family: "Noto Sans CJK TC", sans-serif;
   width: 320px;
-  margin:0;
-`
+  margin: 0;
+`;
 
 const CheckboxWrapper = styled.section`
-    * {
-      font-style: normal;
-      font-weight: 400;
-      font-size: 18px;
-      line-height: 150%;
-      list-style: none;
-      box-sizing: border-box;
-      margin: 0;
-      font-family: 'Noto Sans CJK TC', sans-serif;
-      padding:0;
-    }
-    border-radius: 6px;
-    border: 1px solid;
-    width: 320px;
-    display: flex;
-    flex-direction: column;
-    padding: 16px 24px 20px;
-    margin: 0 0 24px;
-  `
+  * {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 150%;
+    list-style: none;
+    box-sizing: border-box;
+    margin: 0;
+    font-family: "Noto Sans CJK TC", sans-serif;
+    padding: 0;
+  }
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 24px 20px;
+`;
 
 const CheckboxOptionList = styled.ul`
-    position: relative;
-    margin: 0; 
-    padding: 4px 0;
-    max-height: 240px;
-    overflow-y: auto;       
-`
-
+  position: relative;
+  margin: 0;
+  padding: 4px 0;
+  max-height: 240px;
+  overflow-y: auto;
+`;
 
 const CheckboxOption = styled.li`
   color: #000928;
@@ -52,7 +46,7 @@ const CheckboxOption = styled.li`
   padding: 8px 0;
   color: #000928;
   text-align: left;
-  .container{
+  .container {
     display: block;
     position: relative;
     padding-left: 35px;
@@ -60,14 +54,14 @@ const CheckboxOption = styled.li`
     -webkit-user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
-    user-select: none;  
+    user-select: none;
     input {
       position: absolute;
       opacity: 0;
       cursor: pointer;
       height: 0;
       width: 0;
-      &:checked{
+      &:checked {
         background: red;
       }
     }
@@ -77,35 +71,35 @@ const CheckboxOption = styled.li`
       left: 5px;
       height: 20px;
       width: 20px;
-      border:2px solid rgba(0, 9, 40, 0.3);
+      border: 2px solid rgba(0, 9, 40, 0.3);
       border-radius: 3px;
       &:after {
         content: "";
         position: absolute;
         display: none;
       }
-    }   
-    input:checked ~ .checkmark{
-      background: #04295E;
-      &:after{
+    }
+    input:checked ~ .checkmark {
+      background: #04295e;
+      &:after {
         display: block;
-      }    
+      }
     }
     .checkmark:after {
       top: 0;
       left: 5px;
       width: 5px;
       height: 10px;
-      border: solid  #f6f6f5;
+      border: solid #f6f6f5;
       border-width: 0 3px 3px 0;
       -webkit-transform: rotate(45deg);
       -ms-transform: rotate(45deg);
       transform: rotate(45deg);
-}
+    }
   }
-`
+`;
 
-const defaultTitle = '這是複選題'
+const defaultTitle = "這是複選題";
 
 /**
  *  @param {Object} props
@@ -116,37 +110,42 @@ const defaultTitle = '這是複選題'
  *  @return React.ReactElement
  */
 
-export default function Checkbox({ title = defaultTitle, checkedValue = [], ...props }) {
+export default function Checkbox({
+  title = defaultTitle,
+  checkedValue = [],
+  ...props
+}) {
   const chooseOption = (option) => {
-    if(checkedValue.indexOf(option)> -1){
-      props.onChange(checkedValue.filter((item)=> item !== option))
-    }else{
-      props.onChange(checkedValue.concat(option))
+    if (checkedValue.indexOf(option) > -1) {
+      props.onChange(checkedValue.filter((item) => item !== option));
+    } else {
+      props.onChange(checkedValue.concat(option));
     }
-
-  }
-  const optionItem = props.options.map((option) =>
+  };
+  const optionItem = props.options.map((option) => (
     <CheckboxOption key={`option-${option.id}`}>
       <label
         className='container'
         htmlFor={`option-${option.id}`}
-        onChange={(e => chooseOption(e.target.value))}
-      >{option.name}
-        <input type="checkbox" id={`option-${option.id}`} value={option.value} />
-        <span className="checkmark"></span>
+        onChange={(e) => chooseOption(e.target.value)}
+      >
+        {option.name}
+        <input
+          type='checkbox'
+          id={`option-${option.id}`}
+          value={option.value}
+        />
+        <span className='checkmark'></span>
       </label>
-
-
-    </CheckboxOption>);
+    </CheckboxOption>
+  ));
 
   return (
     <React.Fragment>
       <CheckboxWrapper>
         <Title>{title}</Title>
-        <CheckboxOptionList>
-          {optionItem}
-        </CheckboxOptionList>
+        <CheckboxOptionList>{optionItem}</CheckboxOptionList>
       </CheckboxWrapper>
     </React.Fragment>
-  )
+  );
 }
