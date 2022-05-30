4d03548f6ab06a6f6408330580c1cd6fdd511d85
nickhsine <nickhsine97753017@gmail.com>
2022-05-30 16:17:41 +0800

    refactor(feedback): update react-components/form/comments/comment-item.js

--- packages/feedback/src/react-components/form/comments/comment-item.js
+++ packages/feedback/src/react-components/form/comments/comment-item.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React/* eslint-disable-line */, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 
 import CommentSvg from '../../../static/icon-comment.svg'
 import CommentStrongSvg from '../../../static/icon-comment-strong.svg'
-
 
 const SVGWrapper = styled.div`
   width: 20px;
@@ -40,7 +39,7 @@ const Wrapper = styled.button`
     @media (min-width: 1201px) {
       .normal {
         display: block;
-      }  
+      }
     }
   }
 
@@ -62,7 +61,7 @@ const Header = styled.div`
 const Time = styled.span`
   font-size: 14px;
   line-height: 21px;
-  color: rgba(0, 9, 40, 30%)
+  color: rgba(0, 9, 40, 30%);
 `
 
 const Content = styled.div`
@@ -76,14 +75,14 @@ const Content = styled.div`
     line-height: 32px;
   }
   ${({ contentTooLong, contentExpand }) =>
-    (contentTooLong && !contentExpand) ? `
+    contentTooLong && !contentExpand
+      ? `
       max-height: 216px;
       @media (max-width: 768px) {
         max-height: 96px;
       }
-    ` : ''
-  }
-
+    `
+      : ''}
 `
 
 const Hint = styled.div`
@@ -94,8 +93,8 @@ const Hint = styled.div`
 
 export default function CommentItem({ comment }) {
   const [contentExpand, setContentExpand] = useState(false)
-  const [isHovering, setIsHovering] = useState(false)
-  const [isPressing, setIsPressing] = useState(false)
+  // const [isHovering, setIsHovering] = useState(false)
+  // const [isPressing, setIsPressing] = useState(false)
   const [contentTooLong, setContentTooLong] = useState(false)
   const contentRef = useRef()
 
@@ -119,15 +118,35 @@ export default function CommentItem({ comment }) {
     }
   }
 
-  return <Wrapper contentExpand={contentExpand} onMouseOver={() => { setIsHovering(true) }} onMouseOut={() => { setIsHovering(false) }} onMouseDown={() => { setIsPressing(true) }} onMouseUp={feedbackClickedHandler}>
-    <Header>
-      <Time>{comment.date}</Time>
-      <SVGWrapper>
-        < CommentStrongSvg className="strong" />
-        <CommentSvg className="normal" />
-      </SVGWrapper>
-    </Header>
-    <Content contentTooLong={contentTooLong} contentExpand={contentExpand} ref={contentRef} >{comment.content}</Content>
-    {contentTooLong && !contentExpand && <Hint>展開全部</Hint>}
-  </Wrapper >
+  return (
+    <Wrapper
+      contentExpand={contentExpand}
+      onMouseOver={() => {
+        setIsHovering(true)
+      }}
+      onMouseOut={() => {
+        setIsHovering(false)
+      }}
+      onMouseDown={() => {
+        setIsPressing(true)
+      }}
+      onMouseUp={feedbackClickedHandler}
+    >
+      <Header>
+        <Time>{comment.date}</Time>
+        <SVGWrapper>
+          <CommentStrongSvg className="strong" />
+          <CommentSvg className="normal" />
+        </SVGWrapper>
+      </Header>
+      <Content
+        contentTooLong={contentTooLong}
+        contentExpand={contentExpand}
+        ref={contentRef}
+      >
+        {comment.content}
+      </Content>
+      {contentTooLong && !contentExpand && <Hint>展開全部</Hint>}
+    </Wrapper>
+  )
 }
