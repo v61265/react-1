c75b9d9b8a68542e14fed8c9ecee94927013bad9
nickhsine <nickhsine97753017@gmail.com>
2022-06-02 19:16:54 +0800

    refactor(all): update build-code/client.js. `hydrateRoot` -> `createRoot`

--- packages/feedback/src/build-code/client.js
+++ packages/feedback/src/build-code/client.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime/runtime'
 import buildConst from './constants'
 import React from 'react' // eslint-disable-line
-import { hydrateRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import FeedbackComponent from '../react-components'
 
 const namespace = buildConst.namespace
@@ -14,5 +14,6 @@ if (Array.isArray(dataArr) && dataArr.length > 0) {
   const data = dataArr.shift()
   const { uuid, ...dataOfFeedback } = data
   const container = document.getElementById(uuid)
-  const root = hydrateRoot(container, (<FeedbackComponent {...dataOfFeedback} />))
+  const root = createRoot(container)
+  root.render(<FeedbackComponent {...dataOfFeedback} />)
 }
