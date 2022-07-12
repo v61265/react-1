f530b18fb81b6c85aaaf7604fbcd634fb6fa6616
nickhsine <nickhsine97753017@gmail.com>
2022-07-12 11:19:02 +0800

    refactor(audio-quote-shadow): update dev/entry.js

--- packages/audio-quote-shadow/dev/entry.js
+++ packages/audio-quote-shadow/dev/entry.js
@@ -18,6 +18,29 @@ const mocks = [
       '是我忘記不了香港。',
     ],
   ],
+  [
+    ['./audio-example-2.mp3'],
+    [
+      '我在這邊已經兩年多，我本來有的東西愈來愈失去了。',
+      '首先是我相信過的一些香港手足，都無法再相信。',
+      '然後我的未來，本來就不能掌握，就見一步走一步，',
+      '就算了，我連我的夢想都不敢想。',
+    ],
+  ],
+  [
+    ['./audio-example-3.mp3'],
+    [
+      '我在想，如果我有一個讓我愛的國家，會是什麼感覺？',
+      '我想過會不會是台灣？但現在我覺得很困難。',
+    ],
+  ],
 ]
 
-root.render(<AudioQuoteShadow audioUrls={mocks[0][0]} textArr={mocks[0][1]} />)
+root.render(
+  <div>
+    <div style={{ height: '100vh', backgroundColor: 'pink' }} />
+    <AudioQuoteShadow audioUrls={mocks[0][0]} textArr={mocks[0][1]} />
+    <AudioQuoteShadow audioUrls={mocks[1][0]} textArr={mocks[1][1]} />
+    <AudioQuoteShadow audioUrls={mocks[2][0]} textArr={mocks[2][1]} />
+  </div>
+)
