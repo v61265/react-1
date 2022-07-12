bac1475646cc7be5ded0c6d92f24e6c8962f4413
nickhsine <nickhsine97753017@gmail.com>
2022-07-12 10:31:23 +0800

    refactor(audio-quote-shadow): play audio and quote shadow animation

--- packages/audio-quote-shadow/src/react-components/index.js
+++ packages/audio-quote-shadow/src/react-components/index.js
@@ -1,4 +1,5 @@
 import React/* eslint-disable-line */, { useEffect, useRef, useState } from 'react'
+import _QuoteShadow from './quote-shadow'
 import mockups from './mockups'
 import styled from 'styled-components'
 
@@ -17,9 +18,112 @@ export default function AudioQuoteShadow({
   styles,
   textArr,
 }) {
-  // TODO implement AudioQuoteShadow
+  const defaultDuration = 10 // second
+  const audioRef = useRef(null)
+
+  const [audioOpts, setAudioOpts] = useState({
+    paused: false,
+    canPlay: false,
+    duration: defaultDuration,
+    notice: '',
+  })
+
+  useEffect(() => {
+    const audio = audioRef.current
+    const onLoadedMetadata = () => {
+      setAudioOpts((opts) => {
+        return Object.assign({}, opts, {
+          duration: audio.duration || defaultDuration
+        })
+      })
+    }
+
+    const onCanPlay = () => {
+      setAudioOpts((opts) => {
+        return Object.assign({}, opts, {
+          canPlay: true
+        })
+      })
+    }
+
+    if (audio) {
+      audio.addEventListener('loadedmetadata', onLoadedMetadata)
+      audio.addEventListener('canplay', onCanPlay)
+    }
+
+    // clear event listeners
+    return () => {
+      audio.removeEventListener('loadedmetadata', onLoadedMetadata)
+      audio.removeEventListener('canplay', onCanPlay)
+    }
+  },
+    // `[...audioUrls]` is used to avoid from re-running the above codes
+    [...audioUrls])
+
+  useEffect(() => {
+    const audio = audioRef.current
+    if (!audio) {
+      return
+    }
+    const startPlayPromise = audio.play()
+    startPlayPromise
+    // play successfully
+      .then(() => {
+        setAudioOpts((opts) => Object.assign({}, opts, {
+          // clear notice
+          notice: ''
+        }))
+      })
+    // fail to play
+      .catch(error => {
+        // browser prevent from playing audio before user interactions
+        if (error.name === "NotAllowedError") {
+          setAudioOpts((opts) => Object.assign({}, opts, {
+            notice: '請點選聲音播放鍵'
+          }))
+        } else {
+          // TODO: Handle a load or playback error
+        }
+
+        // stop the audio and quote shadow animation
+        setAudioOpts((opts) => Object.assign({}, opts, {
+          paused: true,
+        }))
+      });
+  })
+
   return (
-    <div>AudioQuoteShadowe</div>
+    <Container
+      className={className}
+    >
+      <audio
+        ref={audioRef}
+        preload={preload}
+      >
+        {audioUrls.map((url, index) => (
+          <source key={`audio_source_${index}`} src={url}></source>
+        ))}
+      </audio>
+      <QuoteShadow
+        textArr={textArr}
+        play={!audioOpts.paused}
+        duration={audioOpts.duration}
+        styles={styles}
+      />
+    </Container>
   )
 }
 
+const QuoteShadow = styled(_QuoteShadow)`
+  max-width: 886px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+`
