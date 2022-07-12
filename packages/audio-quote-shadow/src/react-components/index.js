e56fa988800e5950e37b812e4a8f3ce65c376eae
nickhsine <nickhsine97753017@gmail.com>
2022-07-12 10:12:33 +0800

    refactor(audio-quote-shadow): add typedef.js. Define `Styles` type

--- packages/audio-quote-shadow/src/react-components/index.js
+++ packages/audio-quote-shadow/src/react-components/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 
 /**
  *  @param {Object} opts
+ *  @param {import('./typedef').Styles} [opts.styles]
  *  @param {string[]} opts.audioUrls,
  *  @param {string} [opts.className]
  *  @param {string} [opts.preload='auto'] - 'auto', 'none' or 'metadata'. `preload` attribute of `audio` tag.
@@ -13,6 +14,7 @@ export default function AudioQuoteShadow({
   audioUrls,
   className,
   preload,
+  styles,
   textArr,
 }) {
   // TODO implement AudioQuoteShadow
