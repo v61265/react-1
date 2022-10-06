e20670fb8a66e395f98acc6b5b1c25aff8e50a1b
nickhsine <nickhsine97753017@gmail.com>
2022-10-06 11:53:56 +0800

    refactor(feedback): remove `loadWebpackAssets` and `buildEmbeddedCode` functions BREAKING CHANGE: remove buildEmbeddedCode and loadWebpackAssets functions from src/index.js.
    If we want to generate @readr-media/react-feedback embed codes, the
    alternative is to use @readr-media/react-embed-code-generator pkg.

--- packages/feedback/src/index.js
+++ packages/feedback/src/index.js
@@ -1,11 +1,3 @@
 import Feedback from './react-components'
-import path from 'path'
-import { buildEmbeddedCode } from './build-code'
 
-export default {
-  ReactComponent: Feedback,
-  buildEmbeddedCode,
-  loadWebpackAssets: () => {
-    return require(path.resolve(__dirname, '../dist/webpack-assets.json'))
-  },
-}
+export default Feedback
