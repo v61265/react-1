9bd3367f1a6407c282e959c979a3c05eaf5b8459
nickhsine <nickhsine97753017@gmail.com>
2022-11-03 15:08:24 +0800

    feat(ecg): bump version to 1.2.0-beta.1, build live-blog embed codes

--- packages/embed-code-generator/src/build-code/client.js
+++ packages/embed-code-generator/src/build-code/client.js
@@ -3,6 +3,7 @@ import Feedback from '@readr-media/react-feedback/lib/react-components'
 import Questionnaire from '@readr-media/react-questionnaire/lib/react-components'
 import QAList from '@readr-media/react-qa-list/lib/react-components'
 import Karaoke from '@readr-media/react-karaoke/lib/react-components'
+import LiveBlog from '@readr-media/react-live-blog/lib/react-components'
 import React from 'react' // eslint-disable-line
 import { createRoot } from 'react-dom/client'
 
@@ -23,6 +24,10 @@ const pkgComponentArr = [
   {
     name: 'react-karaoke',
     Component: Karaoke,
+  },
+  {
+    name: 'react-live-blog',
+    Component: LiveBlog,
   },
 ]
 
