22ba564d94fd1e8464f8d517ac58e31aac42561d
Tsuki Akiba <erase2004@gmail.com>
2022-11-08 18:08:45 +0800

    feat(feedback): add theme for politics-tracker

--- packages/feedback/src/themes/index.js
+++ packages/feedback/src/themes/index.js
@@ -70,6 +70,68 @@ const themes = {
       },
     },
   },
+  // 關係資料庫 | 政見追蹤平台
+  'politics-tracker': {
+    breakpoint: {
+      mobile: `(min-width: ${mediaSize.xs}px)`,
+      tablet: `(min-width: ${mediaSize.md}px)`,
+      desktop: `(min-width: ${mediaSize.xl}px)`,
+    },
+    thumbField: {
+      title: {
+        color: '#0F2D35',
+        lineHeight: '1.3',
+        fontWeight: 700,
+        mobile: {
+          fontSize: '16px',
+        },
+        tablet: {
+          fontSize: '18px',
+        },
+      },
+      label: {
+        color: 'rgba(0, 9, 40, 0.87);',
+        lineHeight: '1.5',
+        fontWeight: 500,
+        mobile: {
+          fontSize: '14px',
+        },
+        tablet: {
+          fontSize: '16px',
+        },
+      },
+      thumb: {
+        default: {
+          color: 'rgba(15, 45, 53, 0.3)',
+          borderColor: 'rgba(15, 45, 53, 0.1)',
+          backgrounColor: '#FFFFFF',
+        },
+        hover: {
+          color: 'rgba(15, 45, 53, 0.3)',
+          borderColor: '#B3800D',
+          backgrounColor: '#FFFFFF',
+        },
+        active: {
+          color: '#B3800D',
+          borderColor: 'rgba(15, 45, 53, 0.1)',
+          backgrounColor: '#FFFFFF',
+        },
+      },
+      statistic: {
+        color: 'rgba(15, 45, 53, 0.3)',
+        fontWeight: 500,
+        lineHeight: '24px',
+        mobile: {
+          fontSize: '12px',
+          lineHeight: '14px',
+        },
+        tablet: {
+          fontSize: '14px',
+          lineHeight: '16px',
+        },
+      },
+    },
+  },
 }
 
 export default themes
