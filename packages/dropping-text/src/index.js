8389f0d991fefc0f64b264f7048b57fcec832c55
nickhsine <nickhsine97753017@gmail.com>
2023-05-04 11:48:37 +0800

    refactor(dropping-text): update JSDoc. Add `DroppingTextProps` typedef

--- packages/dropping-text/src/index.js
+++ packages/dropping-text/src/index.js
@@ -1,3 +1,7 @@
 import DroppingText from './react-components/index.js'
 
+/**
+ *  @typedef {import('./react-components/index.js').DroppingTextProps} DroppingTextProps
+ */
+
 export default DroppingText
