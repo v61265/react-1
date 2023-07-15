a26e2c8cfb6838eee6391242df5e8393a63ed691
caesar <caesar.lee@mirrormedia.mg>
2023-07-15 17:04:24 +0800

    refactor(timeline): add dev server

---
+++ packages/timeline/dev/entry.js
@@ -0,0 +1,21 @@
+import 'regenerator-runtime/runtime'
+import React from 'react' // eslint-disable-line
+import lb from '../src'
+import { createRoot } from 'react-dom/client'
+
+const reactRootId = 'root'
+const container = document.getElementById(reactRootId)
+const root = createRoot(container)
+
+async function main() {
+  const ldr = new lb.DataLoader()
+  const data = await ldr.loadData(
+    'https://storage.googleapis.com/editools-gcs-dev.readr.tw/files/liveblogs/timeline.json'
+  )
+  root.render(
+    <>
+      <lb.ReactComponent.Timeline liveblog={data} />
+    </>
+  )
+}
+main()
