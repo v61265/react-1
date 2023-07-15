e0eaabeaeb8f637060dc27a509aec78dcfbbf517
caesar <caesar.lee@mirrormedia.mg>
2023-07-15 17:03:05 +0800

    feat(timeline): add package react-timeline

---
+++ packages/timeline/dev/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './entry.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    hot: false,
+    host: '0.0.0.0',
+    port: 8080,
+    compress: true,
+    static: {
+      directory: path.join(__dirname),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              [
+                '@babel/preset-react',
+                { development: true, runtime: 'automatic' },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'),
+    }),
+  ],
+  devtool: 'eval-source-map',
+}
