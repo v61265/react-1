0ecda2bb4b222f1379f73895183cca315366a846
Xuan <as840922qq@gmail.com>
2023-07-18 23:55:29 +0800

    feat(theatre): add control configs ( font & image )

---
+++ packages/theatre/src/react-components/constants/font.js
@@ -0,0 +1,61 @@
+import { types } from '@theatre/core'
+
+export const FONT = {
+  position: types.compound({
+    x: types.number(40, {
+      range: [0, 100],
+      nudgeMultiplier: 0.5,
+    }),
+    y: types.number(40, {
+      range: [0, 100],
+      nudgeMultiplier: 0.5,
+    }),
+  }),
+  size: types.compound({
+    width: types.number(200, {
+      range: [0, 1200],
+      nudgeMultiplier: 1,
+    }),
+  }),
+  bgColor: types.rgba({ r: 0, g: 0, b: 0, a: 1 }),
+  font: types.compound({
+    size: types.number(16, { range: [0, 300], nudgeMultiplier: 1 }),
+    color: types.rgba({ r: 255, g: 255, b: 255, a: 1 }),
+    weight: types.number(200, { range: [100, 900], nudgeMultiplier: 100 }),
+    spacing: types.number(0.6, { range: [0, 30], nudgeMultiplier: 0.1 }),
+  }),
+  border: types.compound({
+    size: types.number(0),
+    color: types.rgba({ r: 0, g: 0, b: 0, a: 1 }),
+    radius: types.number(4, {
+      range: [0, 100],
+      nudgeMultiplier: 1,
+    }),
+  }),
+  content: types.string(''),
+  opacity: types.number(1, {
+    range: [0, 1],
+    nudgeMultiplier: 0.05,
+  }),
+  padding: types.number(32, {
+    range: [0, 200],
+    nudgeMultiplier: 1,
+  }),
+  visible: types.boolean(true),
+  zIndex: types.stringLiteral('10', {
+    bottom: '-10',
+    0: '0',
+    10: '10',
+    100: '100',
+    1000: '1000',
+    10000: '10000',
+  }),
+  lineHeight: types.number(27, { range: [0, 100], nudgeMultiplier: 1 }),
+  textAlign: types.stringLiteral('justify', {
+    center: 'center',
+    left: 'left',
+    right: 'right',
+    justify: 'justify',
+  }),
+  scale: types.number(1, { range: [0, 100], nudgeMultiplier: 0.1 }),
+}
---
+++ packages/theatre/src/react-components/constants/index.js
@@ -0,0 +1,7 @@
+import { FONT } from './font.js'
+import { IMAGE } from './image.js'
+
+export const initialConfig = {
+  FONT,
+  IMAGE,
+}
