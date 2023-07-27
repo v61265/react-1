9be8bba7e357787b3cdc87d6cf2e6f7e4b76f939
caesar <caesar.lee@mirrormedia.mg>
2023-07-27 11:49:15 +0800

    refactor(timeline): change dev server style

--- packages/timeline/dev/entry.js
+++ packages/timeline/dev/entry.js
@@ -2,10 +2,42 @@ import 'regenerator-runtime/runtime'
 import React from 'react' // eslint-disable-line
 import lb from '../src'
 import { createRoot } from 'react-dom/client'
+import styled from 'styled-components'
 
 const reactRootId = 'root'
 const container = document.getElementById(reactRootId)
 const root = createRoot(container)
+
+const Header = styled.div`
+  background: white;
+  border: 2px solid black;
+  position: fixed;
+  height: 66px;
+  width: 100%;
+  z-index: 10000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 568px) {
+    height: 80px;
+  }
+`
+
+const ContentAbove = styled.div`
+  height: 150vh;
+  background: pink;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const ContentBelow = styled.div`
+  height: 200vh;
+  background: lightblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 async function main() {
   const ldr = new lb.DataLoader()
@@ -14,7 +46,10 @@ async function main() {
   )
   root.render(
     <>
+      <Header>Readr Header</Header>
+      <ContentAbove>Timeline 之前的段落</ContentAbove>
       <lb.ReactComponent.Timeline liveblog={data} />
+      <ContentBelow>Timeline 之後的段落</ContentBelow>
     </>
   )
 }
