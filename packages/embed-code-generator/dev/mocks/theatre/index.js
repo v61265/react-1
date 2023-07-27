ce6549f6a056a5b5bcd2bf97e0eb54110b3edbc5
Xuan <as840922qq@gmail.com>
2023-07-27 10:20:49 +0800

    feat(ecg): update `react-theatre` mock data

--- packages/embed-code-generator/dev/mocks/theatre/index.js
+++ packages/embed-code-generator/dev/mocks/theatre/index.js
@@ -3,21 +3,55 @@ export default {
     sheetsById: {
       Scene: {
         staticOverrides: {
-          byObject: {},
+          byObject: {
+            'font-001': {
+              content: '桌機版測試',
+              bgColor: {
+                r: 0.3215686274509804,
+                g: 0.6549019607843137,
+                b: 0.7450980392156863,
+                a: 1,
+              },
+              size: { width: 266 },
+              border: { size: 4 },
+            },
+          },
         },
         sequence: {
           subUnitsPerUnit: 30,
-          length: 1.5,
+          length: 3.2,
           type: 'PositionalSequence',
           tracksByObject: {
-            test01: {
+            'font-001': {
               trackData: {
-                ODxut07NjS: {
+                xESeviLY8n: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["position","x"]',
+                  __debugName: 'font-001:["position","x"]',
                   keyframes: [
                     {
-                      id: 'N-6BcAaSKY',
+                      id: 'zz9L7Fcr9H',
+                      position: 0,
+                      connectedRight: true,
+                      handles: [0.5, 1, 0.5, 0],
+                      type: 'bezier',
+                      value: 19,
+                    },
+                    {
+                      id: 'o_p_SgkgsK',
+                      position: 2.6,
+                      connectedRight: true,
+                      handles: [0.5, 1, 0.5, 0],
+                      type: 'bezier',
+                      value: 40,
+                    },
+                  ],
+                },
+                '55YLqMG0YQ': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["position","y"]',
+                  keyframes: [
+                    {
+                      id: 'OBgMRBnju-',
                       position: 0,
                       connectedRight: true,
                       handles: [0.5, 1, 0.5, 0],
@@ -25,126 +59,121 @@ export default {
                       value: 40,
                     },
                     {
-                      id: '8WCjWaJoOb',
-                      position: 1.2,
+                      id: 'ptAcF6Zuw8',
+                      position: 2.6,
                       connectedRight: true,
                       handles: [0.5, 1, 0.5, 0],
                       type: 'bezier',
-                      value: 60,
+                      value: 12,
                     },
                   ],
                 },
-                '0Vi6qtdlfn': {
+                I1owiamQpF: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["position","y"]',
+                  __debugName: 'font-001:["size","width"]',
                   keyframes: [],
                 },
-                twa1Se3r4q: {
+                WT6cGzQiUM: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["size","width"]',
+                  __debugName: 'font-001:["bgColor"]',
                   keyframes: [],
                 },
-                jPGdx84fZS: {
+                'Lkcv-KZhTM': {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["bgColor"]',
+                  __debugName: 'font-001:["font","size"]',
                   keyframes: [],
                 },
-                'U-1mnwGabJ': {
+                gSoSSiSjy4: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["font","size"]',
+                  __debugName: 'font-001:["font","color"]',
                   keyframes: [],
                 },
-                JccmqJ6E5D: {
+                '85ydHOpmR8': {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["font","color"]',
+                  __debugName: 'font-001:["font","weight"]',
                   keyframes: [],
                 },
-                '8kkzsxIkzU': {
+                '0WIYod-a5q': {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["font","weight"]',
+                  __debugName: 'font-001:["font","spacing"]',
                   keyframes: [],
                 },
-                oej3iyaqi6: {
+                '713BhDkiXK': {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["font","spacing"]',
+                  __debugName: 'font-001:["border","size"]',
                   keyframes: [],
                 },
-                i3dIo1A4dR: {
+                '0ZnPmtZMuE': {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["border","size"]',
+                  __debugName: 'font-001:["border","color"]',
                   keyframes: [],
                 },
-                'z-yvEz9XzH': {
+                tsjuQdrNpD: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["border","color"]',
+                  __debugName: 'font-001:["border","radius"]',
                   keyframes: [],
                 },
-                '4mNvR1N9Xj': {
+                Wj0dk6UHn7: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["border","radius"]',
+                  __debugName: 'font-001:["content"]',
                   keyframes: [],
                 },
-                '5MV9Ih-PeK': {
+                hWs70cXoSV: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["content"]',
+                  __debugName: 'font-001:["opacity"]',
                   keyframes: [],
                 },
-                'rFNAS-rilC': {
+                bz7_oyi9Zt: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["opacity"]',
+                  __debugName: 'font-001:["padding"]',
                   keyframes: [],
                 },
-                pk3muD4Fnf: {
+                HRB_nb1dJc: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["padding"]',
+                  __debugName: 'font-001:["visible"]',
                   keyframes: [],
                 },
-                'ZXq-bTu9Q8': {
+                chc5Nylrfm: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["visible"]',
+                  __debugName: 'font-001:["zIndex"]',
                   keyframes: [],
                 },
-                '0J6FbeYCu6': {
+                zasf8KpnPb: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["zIndex"]',
+                  __debugName: 'font-001:["lineHeight"]',
                   keyframes: [],
                 },
-                FeneXqraar: {
+                eG_ATRw8Xt: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["lineHeight"]',
+                  __debugName: 'font-001:["textAlign"]',
                   keyframes: [],
                 },
-                '9B5_PW6ESg': {
+                eM4lxnpYLQ: {
                   type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["textAlign"]',
-                  keyframes: [],
-                },
-                UmzsojFGm0: {
-                  type: 'BasicKeyframedTrack',
-                  __debugName: 'test01:["scale"]',
+                  __debugName: 'font-001:["scale"]',
                   keyframes: [],
                 },
               },
               trackIdByPropPath: {
-                '["position","x"]': 'ODxut07NjS',
-                '["position","y"]': '0Vi6qtdlfn',
-                '["size","width"]': 'twa1Se3r4q',
-                '["bgColor"]': 'jPGdx84fZS',
-                '["font","size"]': 'U-1mnwGabJ',
-                '["font","color"]': 'JccmqJ6E5D',
-                '["font","weight"]': '8kkzsxIkzU',
-                '["font","spacing"]': 'oej3iyaqi6',
-                '["border","size"]': 'i3dIo1A4dR',
-                '["border","color"]': 'z-yvEz9XzH',
-                '["border","radius"]': '4mNvR1N9Xj',
-                '["content"]': '5MV9Ih-PeK',
-                '["opacity"]': 'rFNAS-rilC',
-                '["padding"]': 'pk3muD4Fnf',
-                '["visible"]': 'ZXq-bTu9Q8',
-                '["zIndex"]': '0J6FbeYCu6',
-                '["lineHeight"]': 'FeneXqraar',
-                '["textAlign"]': '9B5_PW6ESg',
-                '["scale"]': 'UmzsojFGm0',
+                '["position","x"]': 'xESeviLY8n',
+                '["position","y"]': '55YLqMG0YQ',
+                '["size","width"]': 'I1owiamQpF',
+                '["bgColor"]': 'WT6cGzQiUM',
+                '["font","size"]': 'Lkcv-KZhTM',
+                '["font","color"]': 'gSoSSiSjy4',
+                '["font","weight"]': '85ydHOpmR8',
+                '["font","spacing"]': '0WIYod-a5q',
+                '["border","size"]': '713BhDkiXK',
+                '["border","color"]': '0ZnPmtZMuE',
+                '["border","radius"]': 'tsjuQdrNpD',
+                '["content"]': 'Wj0dk6UHn7',
+                '["opacity"]': 'hWs70cXoSV',
+                '["padding"]': 'bz7_oyi9Zt',
+                '["visible"]': 'HRB_nb1dJc',
+                '["zIndex"]': 'chc5Nylrfm',
+                '["lineHeight"]': 'zasf8KpnPb',
+                '["textAlign"]': 'eG_ATRw8Xt',
+                '["scale"]': 'eM4lxnpYLQ',
               },
             },
           },
@@ -152,8 +181,194 @@ export default {
       },
     },
     definitionVersion: '0.4.0',
-    revisionHistory: ['eoQYCiPyGkiGLjQw'],
+    revisionHistory: [],
   },
-  objectJson: [{ id: 'test01', content: 'test01', type: 'FONT' }],
+  objectJson: [{ id: 'font-001', type: 'FONT' }],
+  mobileAnimateJson: {
+    sheetsById: {
+      Scene: {
+        staticOverrides: {
+          byObject: {
+            'font-001': {
+              position: { x: 25.492227979274613, y: 44.390243902439025 },
+              size: { width: 115 },
+              bgColor: {
+                r: 0.9137254901960784,
+                g: 0.4117647058823529,
+                b: 0.4117647058823529,
+                a: 1,
+              },
+              font: {
+                size: 16,
+                color: { r: 1, g: 1, b: 1, a: 1 },
+                weight: 200,
+                spacing: 0.6,
+              },
+              border: {
+                size: 0,
+                color: { r: 0, g: 0, b: 0, a: 1 },
+                radius: 49,
+              },
+              content: '手機版測試',
+              opacity: 1,
+              padding: 32,
+              visible: true,
+              zIndex: '10',
+              lineHeight: 27,
+              textAlign: 'justify',
+              scale: 1,
+            },
+          },
+        },
+        sequence: {
+          subUnitsPerUnit: 30,
+          length: 2,
+          type: 'PositionalSequence',
+          tracksByObject: {
+            'font-001': {
+              trackData: {
+                ZRsfAwv1zl: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["position","x"]',
+                  keyframes: [],
+                },
+                '4W79tynr6Z': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["position","y"]',
+                  keyframes: [
+                    {
+                      id: '_D-fd-dISv',
+                      position: 0,
+                      connectedRight: true,
+                      handles: [0.5, 1, 0.5, 0],
+                      type: 'bezier',
+                      value: 44.390243902439025,
+                    },
+                    {
+                      id: '96WEGD178k',
+                      position: 1.667,
+                      connectedRight: true,
+                      handles: [0.5, 1, 0.5, 0],
+                      type: 'bezier',
+                      value: 11.890243902439025,
+                    },
+                  ],
+                },
+                '42dabkDGJQ': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["size","width"]',
+                  keyframes: [],
+                },
+                cQesX3YNB1: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["bgColor"]',
+                  keyframes: [],
+                },
+                bC6VF8r3Ri: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["font","size"]',
+                  keyframes: [],
+                },
+                '5aJooJNKi1': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["font","color"]',
+                  keyframes: [],
+                },
+                dblfuOGctC: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["font","weight"]',
+                  keyframes: [],
+                },
+                'V5er1PBX-W': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["font","spacing"]',
+                  keyframes: [],
+                },
+                tnuKJqx_rM: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["border","size"]',
+                  keyframes: [],
+                },
+                nsHcfAp2Sl: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["border","color"]',
+                  keyframes: [],
+                },
+                cpog5sBXQe: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["border","radius"]',
+                  keyframes: [],
+                },
+                HX2zjzTgKq: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["content"]',
+                  keyframes: [],
+                },
+                '1x6DjXU1Mp': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["opacity"]',
+                  keyframes: [],
+                },
+                '67XsLkDT1a': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["padding"]',
+                  keyframes: [],
+                },
+                '7Y6CWbRNu0': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["visible"]',
+                  keyframes: [],
+                },
+                '2cw4LuiySA': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["zIndex"]',
+                  keyframes: [],
+                },
+                zM5odWdHLz: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["lineHeight"]',
+                  keyframes: [],
+                },
+                HGCZ6FITtC: {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["textAlign"]',
+                  keyframes: [],
+                },
+                '7aO2QKbkkw': {
+                  type: 'BasicKeyframedTrack',
+                  __debugName: 'font-001:["scale"]',
+                  keyframes: [],
+                },
+              },
+              trackIdByPropPath: {
+                '["position","x"]': 'ZRsfAwv1zl',
+                '["position","y"]': '4W79tynr6Z',
+                '["size","width"]': '42dabkDGJQ',
+                '["bgColor"]': 'cQesX3YNB1',
+                '["font","size"]': 'bC6VF8r3Ri',
+                '["font","color"]': '5aJooJNKi1',
+                '["font","weight"]': 'dblfuOGctC',
+                '["font","spacing"]': 'V5er1PBX-W',
+                '["border","size"]': 'tnuKJqx_rM',
+                '["border","color"]': 'nsHcfAp2Sl',
+                '["border","radius"]': 'cpog5sBXQe',
+                '["content"]': 'HX2zjzTgKq',
+                '["opacity"]': '1x6DjXU1Mp',
+                '["padding"]': '67XsLkDT1a',
+                '["visible"]': '7Y6CWbRNu0',
+                '["zIndex"]': '2cw4LuiySA',
+                '["lineHeight"]': 'zM5odWdHLz',
+                '["textAlign"]': 'HGCZ6FITtC',
+                '["scale"]': '7aO2QKbkkw',
+              },
+            },
+          },
+        },
+      },
+    },
+    definitionVersion: '0.4.0',
+    revisionHistory: [],
+  },
+  mobileObjectJson: [{ id: 'font-001', type: 'FONT' }],
   type: 'scroll',
 }
