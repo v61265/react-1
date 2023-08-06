7ce00e55b241a33e37e225f512fe05937e1a02ec
Tsuki Akiba <erase2004@gmail.com>
2023-08-06 22:32:22 +0800

    refactor(feedback): add mock data of SingleOptionField

--- packages/feedback/dev/mock-forms.js
+++ packages/feedback/dev/mock-forms.js
@@ -36,5 +36,43 @@ export const formsData = {
         },
       ],
     },
+    {
+      id: '10',
+      name: 'feedback-single',
+      fields: [
+        {
+          id: '14',
+          name: '這段讓你覺得...',
+          type: 'single',
+          options: [
+            {
+              name: '很讚',
+              value: 'good',
+              iconUrl: '/assets/good.svg',
+            },
+            {
+              name: '超愛',
+              value: 'very-good',
+              iconUrl: '/assets/very-good.svg',
+            },
+            {
+              name: '想哭',
+              value: 'sad',
+              iconUrl: '/assets/sad.svg',
+            },
+            {
+              name: '驚訝',
+              value: 'shock',
+              iconUrl: '/assets/shock.svg',
+            },
+            {
+              name: '生氣',
+              value: 'angry',
+              iconUrl: '/assets/angry.svg',
+            },
+          ],
+        },
+      ],
+    },
   ],
 }
