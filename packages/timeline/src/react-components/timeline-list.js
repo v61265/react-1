0d1ab4afe37c38618ae9b759ba661b21b62a1d68
caesar <caesar.lee@mirrormedia.mg>
2023-08-16 19:32:42 +0800

    refactor(timeline): replace TimelineList using react-window

--- packages/timeline/src/react-components/timeline-list.js
+++ packages/timeline/src/react-components/timeline-list.js
@@ -1,4 +1,6 @@
+import { FixedSizeList } from 'react-window'
 import TimelineUnit from './timeline-unit'
+import { useEffect, useRef, useState } from 'react'
 
 function getBubbleLevel(max, count) {
   const levelSize = max / 5
@@ -19,28 +21,54 @@ export default function TimelineList({
   firstTimeUnitKey,
   lastTimeeUnitKey,
 }) {
-  return timeUnitKeys.map((timeUnitKey, i) => {
+  const [listHeight, setListHeight] = useState(800)
+  const divider = dividers[measure]
+  const listItemHeight = listHeight / divider
+  const listRef = useRef(null)
+
+  useEffect(() => {
+    setListHeight(window.innerHeight - headerHeight)
+  }, [headerHeight])
+
+  const Row = ({ index, style }) => {
+    const timeUnitKey = timeUnitKeys[index]
     const events = timeUnitEvents[timeUnitKey] || []
+    const i = index
+
     return (
-      <TimelineUnit
-        eventsCount={events.length}
-        bubbleSizeLevel={getBubbleLevel(timeMax, events.length)}
-        dividers={dividers}
-        bubbleLevelSizesInDivider={bubbleLevelSizesInDivider}
-        key={timeUnitKey + i}
-        onBubbleClick={onBubbleClick.bind(null, timeUnitKey)}
-        onSingleTimelineNodeSelect={onSingleTimelineNodeSelect.bind(
-          null,
-          timeUnitKey
-        )}
-        isFocus={timeUnitKey === focusUnitKey}
-        headerHeight={headerHeight}
-        measure={measure}
-        timeUnitKey={timeUnitKey}
-        isTheFirstOrLastUnit={
-          timeUnitKey === firstTimeUnitKey || timeUnitKey === lastTimeeUnitKey
-        }
-      />
+      <div style={style}>
+        <TimelineUnit
+          eventsCount={events.length}
+          bubbleSizeLevel={getBubbleLevel(timeMax, events.length)}
+          dividers={dividers}
+          bubbleLevelSizesInDivider={bubbleLevelSizesInDivider}
+          key={timeUnitKey + i}
+          onBubbleClick={onBubbleClick.bind(null, timeUnitKey)}
+          onSingleTimelineNodeSelect={onSingleTimelineNodeSelect.bind(
+            null,
+            timeUnitKey
+          )}
+          isFocus={timeUnitKey === focusUnitKey}
+          headerHeight={headerHeight}
+          measure={measure}
+          timeUnitKey={timeUnitKey}
+          isTheFirstOrLastUnit={
+            timeUnitKey === firstTimeUnitKey || timeUnitKey === lastTimeeUnitKey
+          }
+        />
+      </div>
     )
-  })
+  }
+
+  return (
+    <FixedSizeList
+      ref={listRef}
+      width={'100vw'}
+      height={listHeight}
+      itemSize={listItemHeight}
+      itemCount={timeUnitKeys.length}
+    >
+      {Row}
+    </FixedSizeList>
+  )
 }
