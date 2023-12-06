724e17f01a41121ceee0300235fdf28d327884ed
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-12-06 12:57:43 +0800

    fix(ecg): handle unexpected errors caused by repeated hydration

--- packages/embed-code-generator/src/build-code/client.js
+++ packages/embed-code-generator/src/build-code/client.js
@@ -14,6 +14,15 @@ function hydrate(namespace, pkgName, Component) {
       const container = document.getElementById(uuid)
       hydrateRoot(container, <Component {...dataOfComponent} />)
     })
+    // Clean up data to avoid repeated hydration.
+    // Repeated hydration might cause unexpected error, such as:
+    // there are two karaoke embedded codes in the same web page.
+    // The first one embed code script has already hydrated these two embedded codes
+    // on the DOM.
+    // However, the second embed code script will hydrate them again since
+    // `window[namespace][pkgName]` has data.
+    // The second hydration will cause unexpected React errors.
+    window[namespace][pkgName] = []
   }
 }
 
@@ -26,6 +35,8 @@ function render(namespace, pkgName, Component) {
       const root = createRoot(container)
       root.render(<Component {...dataOfComponent} />)
     })
+    // Clean up data to avoid repeated rendering.
+    window[namespace][pkgName] = []
   }
 }
 
