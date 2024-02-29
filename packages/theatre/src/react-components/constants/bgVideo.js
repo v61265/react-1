875922ab3a89c58ab16a0c16208740cf017f2344
Xuan <as840922qq@gmail.com>
2024-02-29 22:35:29 +0800

    feat(theatre): add bg scroll video constants

---
+++ packages/theatre/src/react-components/constants/bgVideo.js
@@ -0,0 +1,30 @@
+import { types } from '@theatre/core'
+
+export const BGVIDEO = {
+  position: types.compound({
+    x: types.number(50, {
+      nudgeMultiplier: 0.5,
+    }),
+    y: types.number(50, {
+      nudgeMultiplier: 0.5,
+    }),
+  }),
+  size: types.compound({
+    width: types.number(100, {
+      nudgeMultiplier: 0.5,
+    }),
+    height: types.number(100, {
+      nudgeMultiplier: 0.5,
+    }),
+  }),
+  visible: types.boolean(true),
+  scale: types.number(1, { nudgeMultiplier: 0.1 }),
+  zIndex: types.stringLiteral('10', {
+    bottom: '-10',
+    0: '0',
+    10: '10',
+    100: '100',
+    1000: '1000',
+    10000: '10000',
+  }),
+}
--- packages/theatre/src/react-components/constants/index.js
+++ packages/theatre/src/react-components/constants/index.js
@@ -2,10 +2,12 @@ import { FONT } from './font.js'
 import { IMAGE } from './image.js'
 import { BACKGROUND } from './background.js'
 import { VIDEO } from './video.js'
+import { BGVIDEO } from './bgVideo.js'
 
 export const initialConfig = {
   FONT,
   IMAGE,
   BACKGROUND,
   VIDEO,
+  BGVIDEO,
 }
